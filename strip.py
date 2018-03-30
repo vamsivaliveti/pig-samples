36eba04d1c18a1475b78e3e6891bf18a9671c246
vamsivaliveti <vamsivaliveti.edu@gmail.com>
2018-03-30 21:35:46 +0530

    Second Revision

--- strip.py
+++ strip.py
@@ -3,4 +3,5 @@
 '''This python script would be invoked by Pig and the data will be streamed through it.
 '''
 import sys; 
+#This is the test line
 print sys.stdin.read().replace('","',',').strip('"')
